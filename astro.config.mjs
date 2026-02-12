2bb98484dd13477c9a119514ddb17f25fe0c1db7
rdlfkz <rdlfkz@gmail.com>
2026-02-13 01:26:14 +0900

    Support for Astro builds.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://rdlfkz.github.io',
     markdown: {
         remarkPlugins: ['remark-breaks'], 
         extendDefaultPlugins: true,
